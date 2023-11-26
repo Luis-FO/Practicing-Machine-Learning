4fa63d152114a1c7b59ccfd4f25f051c64306ab9
Luis-FO <luisfernandophsc@gmail.com>
2023-11-26 11:23:37 -0300

    Just Saving to next step

--- NeuralNetwork/single_neuron_V2.py
+++ NeuralNetwork/single_neuron_V2.py
@@ -9,12 +9,12 @@ class Neuron:
         print("Peso: ", self.__weight)
         #Training data
         # y = ax+b
-        w = 10
-        b = 10
+        w = 2
+        b = 1
 
         self.n = 1000
         self.x = np.arange(1, self.n)
-        self.y = relu(w*self.x + b)
+        self.y = sigmoid(w*self.x + b)
 
     @property
     def bias(self):
@@ -31,14 +31,12 @@ class Neuron:
     def weight(self):
         return self.__weight
 
-    @bias.setter
+    @weight.setter
     def weight(self, new_weight):
         if str(new_weight).isnumeric():
             self.__weight = int(new_weight)
         else:
             print("Informe apenas números")
-
-
 
     def calculate_output(self, x):
         """x is an input"""
@@ -49,8 +47,8 @@ class Neuron:
         n = 1000
         for i in range(n):
             delta_nw, delta_nb = self.batch()
-            self.__weight -= 0.0005*delta_nw/n
-            self.__bias -= 0.05*delta_nb/n
+            self.__weight -= 15000*delta_nw/n
+            self.__bias -= 15000*delta_nb/n
 
 
 
@@ -66,14 +64,14 @@ class Neuron:
         print("B: ", self.__bias) 
         print("Delta ww", -delta_nw/self.n)
         print("Delta bb", -delta_nb/self.n)
-        input()
+        #input()
 
         return (delta_nw/self.n, delta_nb/self.n)
     
     def backprop(self, x, y):
         z = self.__weight*x + self.__bias
-        activation = relu(z)
-        delta = (activation - y)*relu_prime(z)
+        activation = sigmoid(z)
+        delta = (activation - y)*sigmoid_prime(z)
         nabla_b =  delta
         nabla_w =  delta*x
         return nabla_w, nabla_b
@@ -87,8 +85,9 @@ class Neuron:
 def relu(z):
     return np.maximum(0,z)
 
+
 def relu_prime(x):
-    return 0 if x < 0 else 1
+    return np.where(x > 0, 1, 0)
 
 def sigmoid(z):
 
